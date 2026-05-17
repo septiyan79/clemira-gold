b8011bf668576aa775582b90cdc954f1891f0ae9
Mizoku <septiyan79.se@gmail.com>
2026-05-17 19:39:29 +0700

    migration to nextjs, initial nextjs installation

--- src/components/public/Footer.js
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-  return (
-
-  	<footer className="footer text-faded text-center py-5">
-	    <div className="container"><p className="m-0 small">Copyright &copy; Clemira Gold 2025</p></div>
-	</footer>
-
-  );
- }
--- src/nAdmin/components/Footer.js
+++
@@ -1,8 +0,0 @@
-export default function Footer() {
-    return (
-        <footer className="footer text-center">
-            All Rights Reserved by Nice admin. Designed and Developed by
-            <a href="https://wrappixel.com">WrapPixel</a>.
-        </footer>
-    )
-}
